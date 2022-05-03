7d02cd0256b2b8567b9326e121de846b6d75db23
Szymon Szumski <szy.szumski@gmail.com>
2022-05-04 00:58:56 +0200

    feat: editted index file - added react router, change do TS file and changed style file to node-sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Movie from './components/Movie/Movie';
+import Movies from './components/Movies/Movies';
+import NotFound404 from './components/NotFound404/NotFound404';
+import Header from './layouts/Header/Header';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='movies' element={<Movies />} />
+          <Route path='movies/:id' element={<Movie />} />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
