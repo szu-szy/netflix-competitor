e7f09cc378a2496c233c1d8523ea3316e7f1004b
Szymon Szumski <szy.szumski@gmail.com>
2022-05-04 00:57:59 +0200

    feat: editted App file and changed extension to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Link to='/movies'>Go to movies</Link>
     </div>
   );
 }
